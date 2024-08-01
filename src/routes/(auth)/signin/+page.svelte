71eadcf43568d9a417079181c076ff734494d4fd
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2024-08-01 14:53:29 +0530

    Minor Route Updated

---
+++ src/routes/(auth)/signin/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { ChevronLeftIcon } from 'lucide-svelte';
+</script>
+
+<svelte:head>
+	<title>Sign In | Svee UI</title>
+	<meta name="description" content="Sign In for Svee UI" />
+</svelte:head>
+
+<div class="container flex h-screen w-screen flex-col items-center justify-center">
+	<Button href="/" class="absolute left-4 top-4 md:left-8 md:top-8">
+		<ChevronLeftIcon class="mr-2 size-4" />
+		Back
+	</Button>
+	<div class="mx-auto flex w-full flex-col justify-center gap-6 sm:w-[350px]">
+		<div class="flex flex-col gap-2 text-center">
+			<!-- {/* <Icons.logo class="mx-auto h-6 w-6" /> */} -->
+			<h1 class="text-2xl font-semibold tracking-tight">Welcome back</h1>
+			<p class="text-sm text-muted-foreground">Login to your account</p>
+		</div>
+		<!-- Form -->
+		<p class="px-8 text-center text-sm text-muted-foreground">
+			<a href="/signin" class="hover:text-brand underline underline-offset-4">
+				Don&apos;t have an account? Sign Up
+			</a>
+		</p>
+	</div>
+</div>
